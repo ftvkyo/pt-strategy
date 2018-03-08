edbe4e6fa217eda9707be43f63284aa25cc5b79a
Stepanenko Andrey <ftvkyo2011@yandex.ru>
2018-03-09 02:05:08 +0300

    Added gradle dependencies for LWJGL.
    (It works)

---
+++ src/main/java/Game.java
@@ -0,0 +1,8 @@
+import org.lwjgl.Version;
+
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("Hello everyone!");
+        System.out.println("LWJGL Version " + Version.getVersion() + " is working.");
+    }
+}
