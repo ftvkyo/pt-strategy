fd6b506a0df2ee080a9234d3186b3fecc9cf747f
Natalia Sakovets <tyrefrtter@gmail.com>
2018-03-18 21:43:44 +0300

    Empty docstrings added

--- src/main/java/Model/Landscape/GenericLandscape.java
+++ src/main/java/Model/Landscape/GenericLandscape.java
@@ -13,32 +13,58 @@ abstract public class GenericLandscape {
     private int passableness;
 
 
+    /**
+     *
+     */
     private IUnit unit;
 
 
+    /**
+     *
+     */
     private ArrayList<GenericItem> items;
 
 
+    /**
+     *
+     * @param p
+     */
     void setPassableness(int p) {
         passableness = p;
     }
 
 
+    /**
+     *
+     * @return
+     */
     public int getPassableness() {
         return passableness;
     }
 
 
+    /**
+     *
+     * @param un
+     */
     public void setUnit(IUnit un) {
         this.unit = un;
     }
 
 
+    /**
+     *
+     * @return
+     */
     public IUnit getUnit() {
         return this.unit;
     }
 
 
+    /**
+     *
+     * @param l
+     */
     public void swapUnit(GenericLandscape l) {
         IUnit tmp = l.getUnit();
         l.setUnit(this.getUnit());
@@ -46,23 +72,42 @@ abstract public class GenericLandscape {
     }
 
 
+    /**
+     *
+     * @param item
+     */
     public void addItem(GenericItem item) {
         items.add(item);
     }
 
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<GenericItem> getItems() {
         return (ArrayList<GenericItem>) items.clone();
     }
 
 
+    /**
+     *
+     * @param n
+     */
     public void removeItem(int n) {
         items.remove(n);
     }
 
 
+    /**
+     *
+     */
     abstract public static class GenericLandscapeFactory {
 
+        /**
+         *
+         * @return
+         */
         abstract public GenericLandscape createInstance();
     }
 }
