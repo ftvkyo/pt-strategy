1b78974a07b995820a54a932ddb35ff76812894c
Stepanenko Andrey <ftvkyo2011@yandex.ru>
2018-05-08 05:49:48 +0300

    Сделан уползающий треугольник.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
+import Controller.Controller;
+import Model.Model;
+import View.View;
+
 public class Main {
     public static void main(String[] args) {
-        View v = new View();
-        v.run();
+        Model m = new Model();
+
+        try(View v = new View()) {
+            Controller c = new Controller();
+
+            m.addObserver(v);
+
+            v.addController(c);
+
+            c.addModel(m);
+            c.addView(v);
+
+            v.run();
+        }
     }
 }
