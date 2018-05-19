92b9b0d2e6a693f722dec0771b647321d2049cc3
AleksIks <ammalium@mail.ru>
2018-05-20 01:10:00 +0300

    Дописан тест

--- src/test/java/Model/PlayerLastAction/LastActionTest.java
+++ src/test/java/Model/PlayerLastAction/LastActionTest.java
@@ -3,6 +3,7 @@ package Model.PlayerLastAction;
 import Model.Player.PlayerLastAction.CaretakerOfLastAction;
 import Model.Player.PlayerLastAction.LastAction;
 import Model.Unit.UnitAction.AttackAction;
+import Model.Unit.UnitAction.ExampleAction;
 import Model.Unit.UnitAction.IAction;
 import org.junit.Test;
 
@@ -23,6 +24,15 @@ public class LastActionTest {
         caretaker.setRememberedAction(lastAction.saveLastAction());
 
         assertEquals(lastAction.getLastAction(), attack);
+
+        IAction example = ExampleAction.instance;
+
+        lastAction.setLastAction(example);
+        assertEquals(lastAction.getLastAction(), example);
+
+        lastAction.restoreLastAction(caretaker.getLastAction());
+        assertEquals(lastAction.getLastAction(), attack);
+
 
     }
 }
